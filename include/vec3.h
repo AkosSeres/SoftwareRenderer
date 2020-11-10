a67c4ba0ad6c0f26b00f5d6de0c881d39aacc11a
Akos Seres <seresakos2000@gmail.com>
2020-11-10 01:50:48 +0100

    Implement basic Vec3 struct and its functions

---
+++ include/vec3.h
@@ -0,0 +1,28 @@
+#ifndef MATH_VEC3_C
+#define MATH_VEC3_C
+
+#include <stdbool.h>
+
+/**
+ * Structure representing a 3D vector int the
+ * cartesian coordinate system
+ */
+typedef struct {
+  double x, y, z;
+} Vec3;
+
+Vec3 vec3_new(double _x, double _y, double _z);
+Vec3 vec3_copy(Vec3 *src);
+bool vec3_equals(Vec3 *v, Vec3 *w);
+double vec3_length(Vec3 *v);
+double vec3_sqLength(Vec3 *v);
+void vec3_mult(Vec3 *v, double num);
+void vec3_div(Vec3 *v, double num);
+double vec3_dot(Vec3 *v1, Vec3 *v2);
+double vec3_dotSq(Vec3 *v1, Vec3 *v2);
+void vec3_add(Vec3 *v1, Vec3 *v2);
+void vec3_sub(Vec3 *v1, Vec3 *v2);
+double vec3_angle(Vec3 *v, Vec3 *w);
+Vec3 vec3_lerp(Vec3 *v1, Vec3 *v2, double t);
+
+#endif
---
+++ src/vec3.c
@@ -0,0 +1,120 @@
+#include <math.h>
+#include <vec3.h>
+
+/**
+ * Creates a new 3-dimensional vector
+ */
+Vec3 vec3_new(double _x, double _y, double _z) {
+  Vec3 v;
+  v.x = _x;
+  v.y = _y;
+  v.z = _z;
+  return v;
+}
+
+/**
+ * Copies the value of a vector into a new variable and returrns it
+ */
+Vec3 vec3_copy(Vec3 *src) {
+  Vec3 ret;
+  ret.x = src->x;
+  ret.y = src->y;
+  ret.z = src->z;
+  return ret;
+}
+
+/**
+ * Compares two vectors and returns true if they are the same
+ */
+bool vec3_equals(Vec3 *v, Vec3 *w) {
+  return (v->x == w->x) && (v->y == w->y) && (v->z == w->z);
+}
+
+/**
+ * Returns the length (magnitude) of the given vector
+ */
+double vec3_length(Vec3 *v) {
+  return sqrt(v->x * v->x + v->y * v->y + v->z * v->z);
+}
+
+/**
+ * Returns the length of the given vector squared
+ * A lot faster than getting the actual length
+ * Useful for comparing two vetors' lengths
+ */
+double vec3_sqLength(Vec3 *v) {
+  return (v->x * v->x + v->y * v->y + v->z * v->z);
+}
+
+/**
+ * Multiplies the vector by the given number
+ */
+void vec3_mult(Vec3 *v, double num) {
+  v->x *= num;
+  v->y *= num;
+  v->z *= num;
+}
+
+/**
+ * Divides the vector by the given number
+ * When divided by 0, nothing happens
+ */
+void vec3_div(Vec3 *v, double num) {
+  if (num == 0) return;
+  v->x /= num;
+  v->y /= num;
+  v->z /= num;
+}
+
+/**
+ * Returns the dot product of the given vectors
+ */
+double vec3_dot(Vec3 *v1, Vec3 *v2) {
+  return v1->x * v2->x + v1->y * v2->y + v1->z * v2->z;
+}
+
+/**
+ * Returns the square of the dot product of the given vectors
+ */
+double vec3_dotSq(Vec3 *v1, Vec3 *v2) { return pow(vec3_dot(v1, v2), 2); }
+
+/**
+ * Adds the second vector to the first one
+ */
+void vec3_add(Vec3 *v1, Vec3 *v2) {
+  v1->x += v2->x;
+  v1->y += v2->y;
+  v1->z += v2->z;
+}
+
+/**
+ * Subtracts the second vector from the first one
+ */
+void vec3_sub(Vec3 *v1, Vec3 *v2) {
+  v1->x -= v2->x;
+  v1->y -= v2->y;
+  v1->z -= v2->z;
+}
+
+/**
+ * Calculates the angle between the given vectors
+ * Uses their dot products and acos to calculate it
+ * The returned angle is in radians
+ * dotSq is used here to save a very expensive sqrt call
+ */
+double vec3_angle(Vec3 *v, Vec3 *w) {
+  return acos(sqrt((vec3_dotSq(v, w)) / (vec3_sqLength(v) * vec3_sqLength(w))));
+}
+
+/**
+ * Linearly interpolates between two given vectors and returns the newly created
+ * one If t = 1 then the returned vector equals v2 And if t = 0 the returned
+ * vector equals v1 If t is outside [0, 1], the function is extrapolating
+ */
+Vec3 vec3_lerp(Vec3 *v1, Vec3 *v2, double t) {
+  Vec3 ret = vec3_copy(v1);
+  ret.x += (v2->x - v1->x) * t;
+  ret.y += (v2->y - v1->y) * t;
+  ret.z += (v2->z - v1->z) * t;
+  return ret;
+}
