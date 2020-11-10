d2396107f0b7db364a441e6abe15e08471ca08df
Akos Seres <seresakos2000@gmail.com>
2020-11-11 00:22:54 +0100

    Adjust the program for the new library

--- src/main.c
+++ src/main.c
@@ -2,63 +2,25 @@
 #ifdef __EMSCRIPTEN__
 #include <emscripten.h>
 #endif
+#include <ccanvas.h>
 #include <stdbool.h>
 #include <stdlib.h>
 
-SDL_Window *window;
-SDL_Renderer *renderer;
-SDL_Event event;
-SDL_Texture *brush;
-bool quit = false;
-
-void draw() {
-  SDL_SetRenderDrawColor(renderer, 255, 255, 255, SDL_ALPHA_OPAQUE);
-  SDL_RenderClear(renderer);
-
-  SDL_RenderPresent(renderer);
+void draw(CCanvas *cnv) {
+  // Clear canvas before drawing
+  CCanvas_clear(cnv);
 }
 
-void update() {
-  SDL_PollEvent(&event);
-  if (event.type == SDL_QUIT) {
-    quit = true;
+// TODO: make event handling cleaner
+void update(double dt, CCanvas *cnv) {
+  SDL_PollEvent(&(cnv->event));
+  if (cnv->event.type == SDL_QUIT) {
+    CCanvas_quit(cnv);
     return;
   }
 }
 
 int main(int argc, char *argv[]) {
-  SDL_Init(SDL_INIT_VIDEO);
-  SDL_CreateWindowAndRenderer(512, 512, SDL_WINDOW_RESIZABLE, &window,
-                              &renderer);
-  SDL_RenderSetLogicalSize(renderer, 1024, 1024);
-
-  void *pixel;
-  SDL_Rect bRect;
-  int pitch = 1;
-  bRect.x = 0;
-  bRect.y = 0;
-  bRect.w = 1;
-  bRect.h = 1;
-  brush = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_RGBA8888,
-                            SDL_TEXTUREACCESS_STREAMING, 1, 1);
-  SDL_LockTexture(brush, &bRect, &pixel, &pitch);
-  Uint32 *upixel = (Uint32 *)pixel;
-  *upixel = 0;
-  SDL_UnlockTexture(brush);
-
-#ifdef __EMSCRIPTEN__
-  emscripten_set_main_loop(draw, 0, 1);
-#else
-  while (quit == false) {
-    update();
-    draw();
-    SDL_Delay(16);
-  }
-
-  SDL_DestroyRenderer(renderer);
-  SDL_DestroyWindow(window);
-
-  SDL_Quit();
-#endif
+  CCanvas_create(update, draw, 1024, 1024, 512, 512);
   return 0;
 }
