4e602900ef288144b9edf51092302827def5589c
Akos Seres <seresakos2000@gmail.com>
2020-11-11 18:01:50 +0100

    Append new source files to build scripts

--- build_wasm.sh
+++ build_wasm.sh
@@ -1,4 +1,8 @@
 mkdir dest obj
 emcc -c src/main.c -o obj/main.o -I include -s USE_SDL=2
 emcc -c src/ccanvas.c -o obj/ccanvas.o -I include -s USE_SDL=2
-emcc obj/main.o obj/ccanvas.o -o dest/index.html -s USE_SDL=2
+emcc -c src/camera.c -o obj/camera.o -I include -s USE_SDL=2
+emcc -c src/point.c -o obj/point.o -I include -s USE_SDL=2
+emcc -c src/scene.c -o obj/scene.o -I include -s USE_SDL=2
+emcc -c src/vec3.c -o obj/vec3.o -I include -s USE_SDL=2
+emcc obj/main.o obj/ccanvas.o obj/camera.o obj/point.o obj/scene.o obj/vec3.o -o dest/index.html -s USE_SDL=2
