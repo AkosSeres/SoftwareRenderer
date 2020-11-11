9556c8972f750eaee49423a0c417826cdc834344
Akos Seres <seresakos2000@gmail.com>
2020-11-11 22:24:55 +0100

    Use the linear projection

--- src/scene.c
+++ src/scene.c
@@ -5,7 +5,7 @@ void Scene_setCamera(Scene* scene, Camera cam) { scene->cam = cam; }
 void Scene_projectPoints(Scene* scene) {
   for (int i = 0; i < scene->verticesCount; i++) {
     (scene->projectedPoints)[i] =
-        Camera_project(&(scene->cam), &((scene->vertices)[i]));
+        Camera_projectLinear(&(scene->cam), &((scene->vertices)[i]));
   }
 }
 
