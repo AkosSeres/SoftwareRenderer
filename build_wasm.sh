29dc4dcccb15527a82f348e5faa62a76e7f81ce5
Akos Seres <seresakos2000@gmail.com>
2020-11-21 17:50:12 +0100

    Make cleaner page for the wasm build

--- build_wasm.sh
+++ build_wasm.sh
@@ -1,8 +1,9 @@
 mkdir dest obj
+cp ./index.html ./dest/index.html
 emcc -c src/main.c -o obj/main.o -I include -s USE_SDL=2
 emcc -c src/ccanvas.c -o obj/ccanvas.o -I include -s USE_SDL=2
 emcc -c src/camera.c -o obj/camera.o -I include -s USE_SDL=2
 emcc -c src/point.c -o obj/point.o -I include -s USE_SDL=2
 emcc -c src/scene.c -o obj/scene.o -I include -s USE_SDL=2
 emcc -c src/vec3.c -o obj/vec3.o -I include -s USE_SDL=2
-emcc obj/main.o obj/ccanvas.o obj/camera.o obj/point.o obj/scene.o obj/vec3.o -o dest/index.html -s USE_SDL=2
+emcc obj/main.o obj/ccanvas.o obj/camera.o obj/point.o obj/scene.o obj/vec3.o -o dest/index.js -s USE_SDL=2
