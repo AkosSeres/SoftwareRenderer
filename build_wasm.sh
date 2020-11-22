09fd86a7a26dda0dc2eaf2f9f4f699bb6ace6177
Akos Seres <seresakos2000@gmail.com>
2020-11-22 01:24:42 +0100

    Turn on optimisations for the wasm build

--- build_wasm.sh
+++ build_wasm.sh
@@ -6,4 +6,4 @@ emcc -c src/camera.c -o obj/camera.o -I include -s USE_SDL=2
 emcc -c src/point.c -o obj/point.o -I include -s USE_SDL=2
 emcc -c src/scene.c -o obj/scene.o -I include -s USE_SDL=2
 emcc -c src/vec3.c -o obj/vec3.o -I include -s USE_SDL=2
-emcc obj/main.o obj/ccanvas.o obj/camera.o obj/point.o obj/scene.o obj/vec3.o -o dest/index.js -s USE_SDL=2 -s EXPORTED_FUNCTIONS='["_CCanvas_dropEventForSDL","_main"]' -s EXPORTED_RUNTIME_METHODS='["ccall","cwrap"]' --preload-file base_scene.obj
+emcc -O3 --closure 1 obj/main.o obj/ccanvas.o obj/camera.o obj/point.o obj/scene.o obj/vec3.o -o dest/index.js -s USE_SDL=2 -s EXPORTED_FUNCTIONS='["_CCanvas_dropEventForSDL","_main"]' -s EXPORTED_RUNTIME_METHODS='["ccall","cwrap"]' --preload-file base_scene.obj
