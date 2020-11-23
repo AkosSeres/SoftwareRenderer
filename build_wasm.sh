f38c2f30e80f3d4e0dcb3415b89810535343fa2a
Akos Seres <seresakos2000@gmail.com>
2020-11-23 14:31:46 +0100

    Fix file dropping in WASM build

--- build_wasm.sh
+++ build_wasm.sh
@@ -1,9 +1,8 @@
 mkdir dest obj
-cp ./index.html ./dest/index.html
 emcc -c src/main.c -o obj/main.o -I include -s USE_SDL=2
 emcc -c src/ccanvas.c -o obj/ccanvas.o -I include -s USE_SDL=2
 emcc -c src/camera.c -o obj/camera.o -I include -s USE_SDL=2
 emcc -c src/point.c -o obj/point.o -I include -s USE_SDL=2
 emcc -c src/scene.c -o obj/scene.o -I include -s USE_SDL=2
 emcc -c src/vec3.c -o obj/vec3.o -I include -s USE_SDL=2
-emcc -O3 --closure 1 obj/main.o obj/ccanvas.o obj/camera.o obj/point.o obj/scene.o obj/vec3.o -o dest/index.js -s USE_SDL=2 -s EXPORTED_FUNCTIONS='["_CCanvas_dropEventForSDL","_main"]' -s EXPORTED_RUNTIME_METHODS='["ccall","cwrap"]' --preload-file base_scene.obj
+emcc -O3 obj/main.o obj/ccanvas.o obj/camera.o obj/point.o obj/scene.o obj/vec3.o -o dest/index.html --shell-file index.html -s USE_SDL=2 -s EXPORTED_FUNCTIONS='["_CCanvas_dropEventForSDL","_main"]' -s EXPORTED_RUNTIME_METHODS='["ccall","cwrap"]' -s FORCE_FILESYSTEM=1 --preload-file base_scene.obj
